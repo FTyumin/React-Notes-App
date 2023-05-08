d557e84a4f4f6b55d0cb7f983a52f2f4de2008bf
FTyumin <feodor.tjumin28@gmail.com>
2023-05-08 19:11:19 +0300

    Added note saving

--- src/App.js
+++ src/App.js
@@ -20,13 +20,23 @@ const App=() => {
       date:"07/05/2023"
     },
   
-
-
 ])
+
+  const addNote = (text) => {
+    const date = new Date()
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    }
+    
+    const newNotes = [...notes, newNote]
+    setNotes(newNotes)
+  }
 
   return (
     <div className="container">
-      <NotesList notes={notes}/>
+      <NotesList notes={notes} handleAddNote= {addNote}/>
     </div>
   )
 }
--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,14 +1,32 @@
-const AddNote = () => {
+import { useState } from "react";
+
+const AddNote = ({handleAddNote}) => {
+    const[noteText, setNoteText] = useState('')
+
+    const handleChange = (event) => {
+        setNoteText(event.target.value)
+    }
+
+    const handleSave = () => {
+        if(noteText.trim().length > 0) {
+            handleAddNote(noteText)
+            setNoteText('')
+        }
+        
+    }
+
     return(
         <div className="note new">
             <textarea
                 rows='8'
                 cols='10'
                 placeholder="Add a note"
+                value={noteText}
+                onChange={handleChange}
             ></textarea>
             <div className="note-footer">
                 <small>200 characters left</small>
-                <button className="save">Save</button>
+                <button className="save" onClick={handleSave}>Save</button>
             </div>
         </div>
     )
